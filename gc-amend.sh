64434bb3c724c2f02143dfda3a864f73d9400ac5
Janzen Zarzoso <janzen.zarzoso@gmail.com>
2016-10-23 17:01:31 +0800

    Created git commit amend date.

---
+++ gc-amend.sh
@@ -0,0 +1,10 @@
+#/!bin/sh
+
+DAY=$1
+DATE=$2
+YEAR=$3
+MSG=$4
+TIME=$(date "+%H:%M:%S")
+
+echo "GIT_AUTHOR_DATE='$DAY $DATE $TIME $YEAR' GIT_COMMITTER_DATE='$DAY $DATE $TIME $YEAR' git commit -m '$MSG'"
+
